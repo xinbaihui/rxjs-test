f8b4e666435192dff90d3d97787fae5239f90fb2
CORP\xinb1 <ella.xin@email.com>
2021-10-20 21:50:51 +0800

    little change

--- src/app/rxjs/rxjs-routing.module.ts
+++ src/app/rxjs/rxjs-routing.module.ts
@@ -8,10 +8,6 @@ const routes: Routes = [
     path: '', 
     component: RxjsComponent
   },
-  { 
-    path: 'join-creation',
-    component: JoinCreationComponent
-  }
 ];
 
 @NgModule({
--- src/app/rxjs/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs/rxjs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, timer } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -10,6 +11,44 @@ export class RxjsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // defination
+    const onSubscribe = (observer: any) => {
+      let i = 1;
+      const handle = setInterval(() => {
+        console.log('create data i:' + i);
+        observer.next(i++);
+        // if(i>3) {
+        //   observer.complete()                                      ;
+        // }
+      }, 1000);
+      // observer.error("Error");
+      // observer.complete();
+      return {
+        unsubscribe: () => {
+          clearInterval(handle);
+        }
+      }
+    }
+    
+    const source$ = new Observable(onSubscribe);
+    
+    const observer = {
+      next: (val: any) => console.log(val),
+      error: (err: any) => console.log(err),
+      complete: () => console.log("No more data")
+    }
+    
+    const subscription = source$.subscribe(observer);
+
+    setTimeout(() => {
+      subscription.unsubscribe();
+    }, 3500)
+  }
+
+  showTimer() {
+    timer(1000).subscribe((val) => {
+      console.log(val)
+    })
   }
 
 }
